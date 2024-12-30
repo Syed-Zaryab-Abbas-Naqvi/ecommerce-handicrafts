05b10efd758b4233bbc33573997052cf10393313
Syed-Zaryab-Abbas-Naqvi <221104@students.au.edu.pk>
2024-12-30 15:38:58 +0500

    Added frontend code

---
+++ frontend/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+    return (
+        <div>
+            <h1>Handicrafts E-Commerce</h1>
+            <p>Explore and buy beautiful handmade crafts!</p>
+        </div>
+    );
+}
+
+export default App;
